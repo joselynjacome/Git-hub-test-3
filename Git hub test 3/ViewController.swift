d353149dbdf5a16ec2ab243877cce400735476ca
Girls Code <GWC@cs81-ms-crt-15.local>
2019-07-18 11:03:29 -0400

    joselyn made main page

--- Git hub test 3/ViewController.swift
+++ Git hub test 3/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let backgroundImageView = UIImageView()
+    override func viewDidLoad(){
+   
+        func setBackground() {
+            // This adds the backgroundImageView to the subview
+            view.addSubview(backgroundImageView)
+            // This turns off autoresizing
+            backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+            
+            // This pins the background image to the top of the screen. We will do this for all four edges
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+            // Leading is the left side, trailing is the right side.
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            
+            // Set the image of the background view
+            // Update the name in quotation marks to match the name of your background image
+            backgroundImageView.image = UIImage(named: "orange number 2")
+            
+            // Set the background image to be behind all the elements
+            view.sendSubviewToBack(backgroundImageView)
+        }
 
-    override func viewDidLoad() {
         super.viewDidLoad()
+        setBackground()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+                        
 }
 
